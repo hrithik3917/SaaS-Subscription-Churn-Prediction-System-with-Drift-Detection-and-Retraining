9b42064aba8db07d815a7596a1324a4bc61745e1
Hritik Dalvi <hrithikdalavi18@gmail.com>
2026-03-15 17:51:38 -0400

    Connected PostgreSQL database in db.py

---
+++ src/utils/db.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+
+load_dotenv()
+
+DATABASE_URL = URL.create(
+    drivername="postgresql+psycopg2",
+    username=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT")),
+    database=os.getenv("DB_NAME"),
+)
+
+engine = create_engine(DATABASE_URL)
